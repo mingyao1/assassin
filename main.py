789b14c1a5a476b4675ddce2ecd97bedab8acae4
mingyao1 <106688909+mingyao1@users.noreply.github.com>
2023-08-21 11:28:20 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,23 @@
 import csv
+import random
 
 filename = input("Please input the filepath for a CSV containing your target list> ")
+
+players = []
 
 with open(filename, 'r') as f:
   csv = csv.reader(f)
   for row in csv:
     print(', '.join(row))
+    players.add(row)
 
 #insert remaining code here
+
+random.shuffle(players)
+start = players[0]
+
+with open("outfile.csv", 'w') as o:
+  output = csv.writer(o)
+  for i in range(0, len(players)-1):
+    output.writerow(players[i], players[i+1])
+  output.writerow(players[-1], players[0])
